c0124597f994b8e476470f865e4422bbc4d9f705
Ivan Molodetskikh <yalterz@gmail.com>
2020-03-04 10:46:58 +0300

    postinstall: copy schema to the bindir
    The launcher will load its schema from the executable directory. This is
    to allow using the launcher without installing it system-wide.

--- build-aux/meson/postinstall.py
+++ build-aux/meson/postinstall.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from os import environ, path
+from shutil import copy
 from subprocess import call
 
 prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+bindir = path.join(prefix, 'bin')
 datadir = path.join(prefix, 'share')
 destdir = environ.get('DESTDIR', '')
+schema_dir = path.join(datadir, 'glib-2.0', 'schemas')
 
 # Package managers set this so we don't need to run
 if not destdir:
@@ -16,6 +19,6 @@ if not destdir:
     call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
 
     print('Compiling GSettings schemas...')
-    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
-
+    call(['glib-compile-schemas', schema_dir])
+    copy(path.join(schema_dir, 'gschemas.compiled'), bindir)
 
